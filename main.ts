cf8a92d1578ebd7bf5018c4419f16fcbfbe7de1b
aadhyac16 <83731774+aadhyac16@users.noreply.github.com>
2021-06-25 18:41:55 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        # . . . #
+        . # # # .
+        . . . . .
+        # # # # #
+        # . # . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # # # # #
+        . . # . #
+        . . # # #
+        `)
+})
